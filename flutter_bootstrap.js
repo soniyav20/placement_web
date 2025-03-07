abdc55cc484eef9da90dbed769299a756b6597c9
soniyanw <soniya@nativewit.in>
2025-03-07 23:00:24 +0530

    Student,Alumni,Placement Done

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1183368391"
+    serviceWorkerVersion: "2855462354"
   }
 });
